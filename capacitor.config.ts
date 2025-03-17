450668f33d0ec3cbed76287009c4e5eba421d5d6
Aluno <etecae023@hotmail.com>
2025-03-17 19:25:48 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'atividade avaliativa',
+  webDir: 'www'
+};
+
+export default config;
